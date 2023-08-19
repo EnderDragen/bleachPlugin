e418b29f336d821ea46f3ade7439018cb7d7b41a
EnderDragen <71349532+EnderDragen@users.noreply.github.com>
2023-08-18 21:53:45 -0400

    Update AuraParticlesEventListener.java

--- SampleKitsLVER/src/com/enderdragen/samplekitsLVER/particles/AuraParticlesEventListener.java
+++ SampleKitsLVER/src/com/enderdragen/samplekitsLVER/particles/AuraParticlesEventListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -14,12 +15,13 @@ import java.util.Map;
 import java.util.UUID;
 
 public class AuraParticlesEventListener implements Listener {
-	 private final AuraParticlesPlugin plugin;		
+	private final JavaPlugin plugin;
    
-	 public AuraParticlesEventListener(AuraParticlesPlugin  plugin) {
+	public AuraParticlesEventListener(JavaPlugin plugin) {
         this.plugin = plugin;
     }
     public static Map<UUID, Long> cooldownPlayers = new HashMap<>();
+  
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
